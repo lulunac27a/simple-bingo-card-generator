81d1db376664892187db3eb675bc1dc3ed1ddb61
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-09-21 17:07:36 -0500

    Add JavaScript comments

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const generateButton = document.getElementById("generate");
-const bingoTable = document.getElementById("bingo-table");
+const generateButton = document.getElementById("generate"); //button to generate bingo board
+const bingoTable = document.getElementById("bingo-table"); //bingo table
 generateButton.addEventListener("click", () => {
     for (let i = 0; i < 5; i++) {
+        //repeat for each row in bingo board
         let columnNumbers = [];
         let j = 0;
         while (j < 5) {
-            let cellNumber = Math.floor(Math.random() * 15) + i * 15 + 1;
+            //repeat for each column in each row
+            let cellNumber = Math.floor(Math.random() * 15) + i * 15 + 1; //each column has 15 numbers
             if (!columnNumbers.includes(cellNumber)) {
-                columnNumbers.push(cellNumber);
+                //check if other numbers are not in bingo board
+                columnNumbers.push(cellNumber); //add numbers to bingo board
                 bingoTable
                     .querySelector(`#row-${j + 1}`)
                     .querySelector(`#cell-${j + 1}-${i + 1}`).textContent =
